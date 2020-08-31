0679ee2bf84e7f528b8e2478c67bcb70e6a9d0d9
Chris Kitras <chkitras@gmail.com>
2020-08-31 17:31:35 -0600

    Coding standard applied

--- userspace/apps/lab1_helloworld/main.c
+++ userspace/apps/lab1_helloworld/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Prints simple hello message to stdout with netid
+// returns 0 to indicate no errors in execution
 int main() {
   printf("Hello, World from kchristm!\n");
   return 0;
