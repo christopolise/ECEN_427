865fa67c2c5023e457b2e023beaafc86e03a10af
Chris Kitras <chkitras@gmail.com>
2020-09-21 13:31:32 -0600

    Possible solution for the buttons

--- userspace/drivers/buttons/buttons.c
+++ userspace/drivers/buttons/buttons.c
@@ -7,6 +7,10 @@
 #define BUTTONS_ERROR -1
 #define MMAP_OFFSET 0x0
 #define BUTTONS_MMAP_SIZE 0x00010000
+#define IP_IER_OFFSET 0x0128
+#define GIER_OFFSET 0x011C
+#define GPIO_DATA_OFFSET 0x0000
+#define IP_ISR_OFFSET 0x0120
 
 static int fd;
 static int addr;
@@ -26,12 +30,18 @@ int32_t buttons_init(char *devFilePath) {
   }
 
   /* put hardware setup here */
+  // Enabling interrupt in the IPIER
+  generic_write(IP_IER_OFFSET, 0x00000001);
+  buttons_enable_interrupts();
 
   return BUTTONS_SUCCESS;
 }
 
 // Return the current state of the buttons
-uint8_t buttons_read() { return *((volatile uint32_t *)(addr + MMAP_OFFSET)); }
+uint8_t buttons_read() {
+  //
+  return generic_read(GPIO_DATA_OFFSET) & BUTTONS_ALL_MASK;
+}
 
 // Call this on exit to clean up
 void buttons_exit() {
@@ -40,13 +50,36 @@ void buttons_exit() {
 }
 
 // Enable GPIO interrupt output
-void buttons_enable_interrupts() {}
+void buttons_enable_interrupts() {
+  //
+  generic_write(GIER_OFFSET, 0x80000000);
+}
 
 // Disable GPIO interrupt output
-void buttons_disable_interrupts() {}
+void buttons_disable_interrupts() {
+  //
+  generic_write(GIER_OFFSET, 0x00000000);
+}
 
 // Return whether an interrupt is pending
-bool buttons_interrupt_pending() {}
+bool buttons_interrupt_pending() {
+  // Check for a one in bit 0 on IPISR to see if
+  return generic_read(IP_ISR_OFFSET) & 0x01;
+}
 
 // Acknowledge a pending interrupt
-void buttons_ack_interrupt() {}
+void buttons_ack_interrupt() {
+  // Make sure the IPISR knows its okay to receive interrupts again
+  generic_write(IP_ISR_OFFSET, 0x00000001);
+}
+
+// write to a register of the UIO device
+void generic_write(uint32_t offset, uint32_t value) {
+  // the address is cast as a pointer so it can be dereferenced
+  *((volatile uint32_t *)(addr + offset)) = value;
+}
+
+// read from a register of the UIO device
+uint32_t generic_read(uint32_t offset) {
+  return *((volatile uint32_t *)(addr + offset));
+}
